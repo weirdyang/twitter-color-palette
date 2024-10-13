b3a635cd2e683863bd7ba61b738d51c2170ff65c
weirdyang <wy4786@gmail.com>
2024-10-13 17:03:07 +0800

    node-fetch

--- netlify/functions/twitter.js
+++ netlify/functions/twitter.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 require('dotenv').config()
-import axios from 'axios';
+import fetch from 'node-fetch';
 
 const bearer = process.env.BEARER;
 const headers = {
@@ -16,8 +16,9 @@ exports.handler = async function ({ queryStringParameters }) {
 
   try{
     const { q: username } = queryStringParameters;
-    const socialDataResponse = await axios.get(`https://api.socialdata.tools/twitter/user/${username}`,
+    const socialDataResponse = await fetch(`https://api.socialdata.tools/twitter/user/${username}`,
     {
+      method: 'GET',
       headers: {
         'Authorization': `Bearer ${bearer}`,
         'Accept': 'application/json'
